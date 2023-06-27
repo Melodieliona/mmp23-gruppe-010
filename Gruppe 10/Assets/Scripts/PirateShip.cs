9476da43d01e82fd32fe7854a9318b661d0e0319
Thomas Leon Meaney <meaney@cip.ifi.lmu.de>
2023-06-27 23:17:43 +0200

    Improve code when spawning in ships

--- Gruppe 10/Assets/Scripts/PirateShip.cs
+++ Gruppe 10/Assets/Scripts/PirateShip.cs
@@ -3,24 +3,25 @@ using static Unity.Burst.Intrinsics.X86;
 
 public class PirateShip : MonoBehaviour
 {
+    [Header("Attributes")] 
     public float speed = 3f;
 
     private Transform target;
     private int waypointIndex = 0;
 
-    void Start()
+    private void Start()
     {
-        this.target = Waypoints.points[0];
+        target = Waypoints.points[0];
     }
 
     /// <summary>
     /// Moves the ship in the direction of the targeted waypoint.
     /// On reaching the target, the next waypoint is set.
     /// </summary>
-    void Update()
+    private void Update()
     {
         Vector2 direction = (target.position - transform.position).normalized;
-        transform.Translate(direction * speed * Time.deltaTime, Space.World);
+        transform.Translate(direction * (speed * Time.deltaTime), Space.World);
 
         if (Vector2.Distance(target.position, transform.position) <= 0.05f)
         {
@@ -30,20 +31,20 @@ public class PirateShip : MonoBehaviour
 
     /// <summary>
     /// Attempts to get the next waypoint to target.
-    /// If there are no more waypoints available, the ship has reached the treasure chest
-    /// and will therefore be destroyed, invoking <see cref="ShipSpawner.onReachTreasureChest"/>.
+    /// If there are no more waypoints available, the ship has reached the treasure chest and will therefore be destroyed,
+    /// invoking <see cref="ShipSpawner.ReachTreasureChestEvent"/>.
     /// </summary>
     private void GetNextWaypoint()
     {
-        this.waypointIndex++;
+        waypointIndex++;
 
         if (waypointIndex >= Waypoints.points.Length)
         {
-            ShipSpawner.onReachTreasureChest.Invoke();
+            ShipSpawner.ReachTreasureChestEvent.Invoke();
             Destroy(gameObject);
             return;
         }
 
-        this.target = Waypoints.points[waypointIndex];
+        target = Waypoints.points[waypointIndex];
     }
 }
--- Gruppe 10/Assets/Scripts/PlayerManager.cs
+++ Gruppe 10/Assets/Scripts/PlayerManager.cs
@@ -1,26 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-
     public int goldCount = 50;
     public int score = 0;
     public int currentWave = 0;
     public int healthPoints = 10;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void AddGold(int number)
     {
@@ -41,14 +26,14 @@ public class PlayerManager : MonoBehaviour
 
     public void AddHP(int number)
     {
-        healthPoints = healthPoints + number;
+        healthPoints += number;
     }
 
     public void RemoveHP(int number)
     {
         if (healthPoints - number > 0)
         {
-            healthPoints = healthPoints - number;
+            healthPoints -= number;
         }
         else
         {
