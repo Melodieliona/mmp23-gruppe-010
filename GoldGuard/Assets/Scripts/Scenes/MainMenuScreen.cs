bd17b206da5b5ec93e167285f7d2e08b39f3b65c
Petra Murr <Petra_murr@hotmail.com>
2023-07-07 19:03:12 +0200

    Placeholdersounds for MainMenu and Game there is still a delay with the canon sound volume not adjusted

--- GoldGuard/Assets/Scripts/Scenes/MainMenuScreen.cs
+++ GoldGuard/Assets/Scripts/Scenes/MainMenuScreen.cs
@@ -5,14 +5,24 @@ namespace Scenes
 {
     public class MainMenuScreen : MonoBehaviour
     {
+        SoundEffectsPlayer soundEffect;
+
+        private void Awake()
+        {
+            soundEffect = GameObject.FindGameObjectWithTag("Audio").GetComponent<SoundEffectsPlayer>();
+        }
+
         public void PlayButton()
         {
+            DontDestroyOnLoad(soundEffect);
+            soundEffect.PlaySFX(soundEffect.button);
             SceneManager.LoadScene("Game");
         }
 
         public void QuitButton()
         {
             Application.Quit();
+            soundEffect.PlaySFX(soundEffect.button);
             Debug.Log("Quit Application");
         }
     }
--- GoldGuard/Assets/Scripts/Sound/SoundEffectsPlayer.cs
+++ GoldGuard/Assets/Scripts/Sound/SoundEffectsPlayer.cs
@@ -1,26 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-namespace Sound
+public class SoundEffectsPlayer : MonoBehaviour
 {
-    public class SoundEffectsPlayer : MonoBehaviour
+    [Header("--------------- Audio Source -----------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("--------------- Audio Clip -----------")]
+    public AudioClip button;
+    public AudioClip background;
+    public AudioClip settingTime;
+    public AudioClip timer;
+    public AudioClip hp;
+    public AudioClip cannon;
+
+    private bool isBackgroundMusicLooping = true;
+    private float fadeDuration = 5f;
+
+
+    private void Start()
     {
-        [Header("Audio Source")]
-        [SerializeField] private AudioSource musicSource;
-        [SerializeField] private AudioSource sfxSource;
+        //PlayBackgroundMusic(settingTime);
+        musicSource.clip = background;
+        musicSource.Play();
+    }
 
-        [Header("Audio Clip")]
-        public AudioClip button;
-        public AudioClip mainBackground;
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
 
-        private void Start()
+    public void PlayBackgroundMusic(AudioClip clip)
+    {
+        musicSource.Stop();
+        musicSource.clip = clip;
+        musicSource.loop = isBackgroundMusicLooping;
+        musicSource.Play();
+    }
+
+    public void ChangeBackgroundMusic(AudioClip clip)
+    {
+        StartCoroutine(FadeOutMusic(clip));
+    }
+
+    private IEnumerator FadeOutMusic(AudioClip newClip)
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < fadeDuration)
         {
-            musicSource.clip = mainBackground;
-            musicSource.Play();
+            float fadeFactor = elapsedTime / fadeDuration;
+            musicSource.volume = 1f - fadeFactor;
+            elapsedTime += Time.deltaTime;
+            yield return null;
         }
 
-        public void PlaySfx(AudioClip clip)
+        musicSource.Stop();
+        musicSource.clip = newClip;
+        musicSource.Play();
+
+        StartCoroutine(FadeInMusic(newClip));
+    }
+
+    private IEnumerator FadeInMusic(AudioClip clip)
+    {
+        musicSource.clip = clip;
+        musicSource.volume = 0f;
+        musicSource.Play();
+
+        float elapsedTime = 0f;
+        while (elapsedTime < fadeDuration)
         {
-            sfxSource.PlayOneShot(clip);
+            float fadeFactor = elapsedTime / fadeDuration;
+            musicSource.volume = fadeFactor;
+            elapsedTime += Time.deltaTime;
+            yield return null;
         }
+
+        musicSource.volume = 1f;
+    }
+
+    public void ToggleBackgroundMusicLoop()
+    {
+        isBackgroundMusicLooping = !isBackgroundMusicLooping;
+        musicSource.loop = isBackgroundMusicLooping;
     }
 }
