36bd05abe751fa4db05d5ed97a3317807a2ffcc7
Thomas Meaney <thomas.meaney@icloud.com>
2023-07-09 17:47:25 +0200

    Increase price of golden cannon

--- GoldGuard/Assets/Scripts/Player/ShopItems/Cannon3Item.cs
+++ GoldGuard/Assets/Scripts/Player/ShopItems/Cannon3Item.cs
@@ -3,7 +3,7 @@ namespace Player.ShopItems
     //TODO
     public class Cannon3Item : ShopItem
     {
-        public Cannon3Item() : base("Cannon3", 3, 50, TileType.Land, 7f)
+        public Cannon3Item() : base("Cannon3", 3, 75, TileType.Land, 7f)
         {
         }
     }
